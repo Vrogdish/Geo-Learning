6efc8697b24adfe283b9eeff0ca23518834ef2ce
Vrogdish <vrogdish@outlook.com>
2023-10-02 14:10:16 +0200

    update footer

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className='h-20 w-full shadow-light'>
-        test
+    <footer className=" w-full shadow-light py-10 px-32 flex justify-between">
+      <div className="">
+        <p>Site developpé par Cédric G. </p>
+      </div>
+      <div className="flex gap-6">
+        <a href="https://github.com/Vrogdish" target="_blank">
+          <Image
+            src={"/images/github.png"}
+            alt="github"
+            width={30}
+            height={30}
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/cedric-gache-12628a266" target="_blank">
+          <Image
+            src={"/images/linkedin.png"}
+            alt="linkedin"
+            width={30}
+            height={30}
+          />
+        </a>
+        <a href="https://twitter.com/Vrogdish" target="_blank">
+          <Image
+            src={"/images/twitter.png"}
+            alt="twitter"
+            width={30}
+            height={30}
+          />
+        </a>
+      </div>
     </footer>
-  )
+  );
 }
